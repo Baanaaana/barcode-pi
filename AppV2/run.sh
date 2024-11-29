6cb1de208ec8c8cd0d4d4598e3c88ba4af7758e0
Baanaaana <rene@rrcommerce.com>
2024-11-29 14:01:02 +0100

    update running version

--- AppV2/run.sh
+++ AppV2/run.sh
@@ -2,5 +2,7 @@
 cd /home/pi/Desktop/AppV2
 source ~/barcode_env/bin/activate
 export DISPLAY=:0
-export PYTHONPATH=/home/pi/Desktop/AppV2
+export PYTHONPATH=/home/pi/Desktop/AppV2:/usr/lib/python3/dist-packages
+export QT_QPA_PLATFORM=xcb
+pyuic5 -x neo_bar.ui -o neo_bar.py
 python3 YesBarcode.py

888eb78baef8fc72ae41272f1f06f6040fc7d772
Baanaaana <rene@rrcommerce.com>
2024-12-06 16:16:49 +0100

    update

--- install_barcode_app.sh
+++ install_barcode_app.sh
@@ -182,7 +182,7 @@ chmod +x ~/barcode-pi/setup_zebra_printer.sh
 chmod +x ~/barcode-pi/verify_printer.py
 chmod +x ~/barcode-pi/remove_printers.sh
 chmod +x ~/barcode-pi/setup_printnode.sh
-chmod +x ~/barcode-pi/setup_printnode_service/sh
+chmod +x ~/barcode-pi/setup_printnode_service.sh
 
 echo "Installation complete!"
 echo "The application will start automatically on next boot"

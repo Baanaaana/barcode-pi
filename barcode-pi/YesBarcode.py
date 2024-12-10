96e943185698ac140f5e366dfb24443e59fcc71d
Baanaaana <rene@rrcommerce.com>
2024-12-10 22:46:53 +0100

    update

--- barcode-pi/YesBarcode.py
+++ barcode-pi/YesBarcode.py
@@ -418,7 +418,7 @@ class MainWindow_exec(QtWidgets.QMainWindow, Ui_MainWindow):
             print('Printing disabled')
 
         self.read_product.clear()
-        self.label_barcode.setPixmap(QtGui.QPixmap("/home/pi/barcode-pi/product-barcode.png"))
+        self.label_barcode.setPixmap(QtGui.QPixmap("/home/pi/barcode-pi/location-qrcode.png"))
         # self.label_barcode.update_label()
         self.spin_copies.setValue(1)
         self.read_product.setFocus(True)
